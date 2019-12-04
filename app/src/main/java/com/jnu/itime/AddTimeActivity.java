2299e0f32c15890ac16e01e5ec0f106d64ca6c1e
itime he <1176013316@qq.com>
2019-12-04 22:23:52 +0800

    设置EditText为空文本时显示内容以及日期时间pickerdialog

--- app/src/main/java/com/jnu/itime/AddTimeActivity.java
+++ app/src/main/java/com/jnu/itime/AddTimeActivity.java
@@ -3,12 +3,22 @@ package com.jnu.itime;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class AddTimeActivity extends AppCompatActivity {
+    private int nowYear, nowMonth, nowDay, nowHour, nowMinute;
+    private String dateText,timeText;
+    private TextView testTimeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +26,40 @@ public class AddTimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_time);
 
         Toolbar mToolbar = findViewById(R.id.mToolbar);
-        //mToolbar.setNavigationIcon(R.drawable.ic_launcher_background);
+        testTimeTextView = (TextView)findViewById(R.id.test_text_view);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左侧添加一个默认的返回图标
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        Calendar ca = Calendar.getInstance();
+        nowYear = ca.get(Calendar.YEAR);
+        nowMonth = ca.get(Calendar.MONTH);
+        nowDay = ca.get(Calendar.DAY_OF_MONTH);
+        nowHour = ca.get(Calendar.HOUR_OF_DAY);
+        nowMinute = ca.get(Calendar.MINUTE);
+
+        testTimeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(AddTimeActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                timeText = String.format(" %d:%d",hourOfDay,minute);
+                                testTimeTextView.setText(dateText + " " + timeText);
+                            }
+                        }, nowHour, nowMinute, true);
+                timePickerDialog.show();
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddTimeActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        dateText = String.format("%d年%d月%d日",year,monthOfYear+1,dayOfMonth);
+                    }
+                }, nowYear, nowMonth, nowDay);
+                datePickerDialog.show();
+            }
+        });
     }
 
     @Override
