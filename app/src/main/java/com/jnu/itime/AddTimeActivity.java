ddcb1df26930d315c7b7059559644486bb071c78
itime he <1176013316@qq.com>
2019-12-08 00:53:54 +0800

    添加了从相册选取图片的功能

--- app/src/main/java/com/jnu/itime/AddTimeActivity.java
+++ app/src/main/java/com/jnu/itime/AddTimeActivity.java
@@ -1,24 +1,49 @@
 package com.jnu.itime;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Calendar;
 
 public class AddTimeActivity extends AppCompatActivity {
+    public static final int RC_CHOOSE_PHOTO = 901;
     private int nowYear, nowMonth, nowDay, nowHour, nowMinute;
+    private int setYear, setMonth, setDay, setHour, setMinute;
+    private EditText addTitleText,addDescriptionText;
+    private String uri = "null";
     private String dateText,timeText;
-    private TextView testTimeTextView;
+    private TextView testTimeTextView, testPicTextView;
+    private ImageView selectImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +51,38 @@ public class AddTimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_time);
 
         Toolbar mToolbar = findViewById(R.id.mToolbar);
+
+        addTitleText = (EditText)findViewById(R.id.add_title_text);
+        addDescriptionText = (EditText)findViewById(R.id.add_description_text);
         testTimeTextView = (TextView)findViewById(R.id.test_text_view);
+        testPicTextView = (TextView)findViewById(R.id.test_pic_text_view);
+        selectImage = (ImageView)findViewById(R.id.add_image_view);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左侧添加一个默认的返回图标
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.add_time_toolbar_comfirm:
+                        Intent intent = new Intent();
+                        intent.putExtra("year", setYear);
+                        intent.putExtra("month", setMonth);
+                        intent.putExtra("day", setDay);
+                        intent.putExtra("hour", setHour);
+                        intent.putExtra("minute", setMinute);
+                        intent.putExtra("imageUri",uri);
+                        intent.putExtra("title", addTitleText.getText().toString());
+                        intent.putExtra("description", addDescriptionText.getText().toString());
+                        setResult(RESULT_OK,intent);
+                        AddTimeActivity.this.finish();
+                        break;
+                }
+                return false;
+            }
+        });
 
         Calendar ca = Calendar.getInstance();
         nowYear = ca.get(Calendar.YEAR);
@@ -38,26 +91,53 @@ public class AddTimeActivity extends AppCompatActivity {
         nowHour = ca.get(Calendar.HOUR_OF_DAY);
         nowMinute = ca.get(Calendar.MINUTE);
 
+        setYear = ca.get(Calendar.YEAR);
+        setMonth = ca.get(Calendar.MONTH)+1;
+        setDay = ca.get(Calendar.DAY_OF_MONTH);
+        setHour = 0;
+        setMinute = 0;
+
         testTimeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TimePickerDialog timePickerDialog = new TimePickerDialog(AddTimeActivity.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                timeText = String.format(" %d:%d",hourOfDay,minute);
-                                testTimeTextView.setText(dateText + " " + timeText);
-                            }
-                        }, nowHour, nowMinute, true);
-                timePickerDialog.show();
-
                 DatePickerDialog datePickerDialog = new DatePickerDialog(AddTimeActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         dateText = String.format("%d年%d月%d日",year,monthOfYear+1,dayOfMonth);
+                        setYear = year;
+                        setMonth = monthOfYear+1;
+                        setDay = dayOfMonth;
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(AddTimeActivity.this,
+                                new TimePickerDialog.OnTimeSetListener() {
+                                    @Override
+                                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                        timeText = String.format(" %d:%d",hourOfDay,minute);
+                                        testTimeTextView.setText(dateText + " " + timeText);
+                                        setHour = hourOfDay;
+                                        setMinute = minute;
+                                    }
+                                }, nowHour, nowMinute, true);
+                        timePickerDialog.show();
                     }
                 }, nowYear, nowMonth, nowDay);
                 datePickerDialog.show();
+            }
+        });
+
+        testPicTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //动态申请获取访问 读写磁盘的权限
+                if (ContextCompat.checkSelfPermission(AddTimeActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(AddTimeActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+                }
+
+                //打开相册
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                //Intent.ACTION_GET_CONTENT = "android.intent.action.GET_CONTENT"
+                intent.setType("image/*");
+                startActivityForResult(intent, RC_CHOOSE_PHOTO); // 打开相册
             }
         });
     }
@@ -75,5 +155,22 @@ public class AddTimeActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = null;
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                uri = imageUri.toString();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                bitmap = BitmapFactory.decodeStream(imageStream);
+                selectImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
